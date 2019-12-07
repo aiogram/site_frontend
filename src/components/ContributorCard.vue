fbc4485cb726f68616f66a507dfe09b32f92c625
Alex Root Junior <jroot.junior@gmail.com>
2019-12-07 23:55:22 +0200

    Lazy-load for contributor avatars

--- src/components/ContributorCard.vue
+++ src/components/ContributorCard.vue
@@ -8,7 +8,22 @@
     <v-img
       :src="avatar"
       :alt="username"
-    />
+      lazy-src="https://github.com/identicons/jasonlong.png"
+    >
+      <template v-slot:placeholder>
+        <v-row
+          class="fill-height ma-0"
+          align="center"
+          justify="center"
+        >
+          <v-progress-circular
+            size="50"
+            color="primary"
+            indeterminate
+          />
+        </v-row>
+      </template>
+    </v-img>
     <v-card-title>{{ username }}</v-card-title>
     <v-card-subtitle>Contributions: {{ contributions }}</v-card-subtitle>
     <v-card-text v-if="description">{{ description }}</v-card-text>
