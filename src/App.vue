4f6ab80bbc4626126de4f9d1608cc0ed920dcd6a
Alex Root Junior <jroot.junior@gmail.com>
2019-12-07 20:51:29 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <NavBar/>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import NavBar from "./components/NavBar.vue";
+
+  export default {
+    name: 'App',
+
+    components: {
+      NavBar,
+    },
+
+    data: () => ({
+      //
+    }),
+  };
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('@/assets/logo.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to aiogram
+        </h1>
+        <p class="subheading font-weight-regular mx-3">
+          <strong>aiogram</strong> is modern and fully asynchronous framework
+          <br/>for Telegram Bot API written in Python 3.7 with asyncio and aiohttp.
+          <br/>It helps you to make your bots faster and simpler.
+        </p>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <p><strong>aiogram</strong> is currently have {{ versions.length }} active versions:</p>
+
+        <v-layout justify-center>
+          <v-tooltip
+            v-for="(next, i) in versions"
+            :key="i"
+            bottom
+          >
+            <template v-slot:activator="{ on }">
+              <a
+                :href="next.documentation"
+                class="subheading mx-3"
+                target="_blank"
+                v-on="on"
+              >
+                {{ next.label }}
+              </a>
+            </template>
+            <span>{{ next.tooltip }}</span>
+          </v-tooltip>
+
+        </v-layout>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other <strong>aiogram</strong> developers,
+          <br>please visit our online
+          <a href="https://t.me/aiogram" target="_blank">english</a> or <a href="https://t.me/aiogram" target="_blank">russian</a> communities.
+        </p>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h3>Special thanks</h3>
+        <p>
+          Special thanks for
+          <router-link to="/contributors">our contributors</router-link> and
+          <a href="https://opencollective.com/aiogram#section-contributors">financial contributors</a>.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data: () => ({
+      versions: [
+        {
+          documentation: 'https://aiogram.readthedocs.io',
+          label: '2.4',
+          tooltip: 'Latest',
+        },
+        {
+          documentation: 'https://aiogram.2038.io',
+          label: '3.0a0',
+          tooltip: 'Work in progress. Is not ready for production.',
+        },
+      ]
+    }),
+  };
+</script>
