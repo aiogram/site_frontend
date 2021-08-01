41faa25bfa7c1225f2d5bd8f6cbee831b68c30d4
Alex Root Junior <jroot.junior@gmail.com>
2021-08-02 00:42:09 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -89,12 +89,12 @@
       versions: [
         {
           documentation: 'https://docs.aiogram.dev',
-          label: '2.13',
+          label: '2.14.2',
           tooltip: 'Latest',
         },
         /* {
           documentation: 'https://dev-docs.aiogram.dev/',
-          label: '3.0a5',
+          label: '3.0a15',
           tooltip: 'Work in progress. Is not ready for production.',
         }, */
       ]
